76c5b043a77914495e388f3478f672fd1acbbc0e
Palani-SN <psn396@gmail.com>
2021-09-19 19:52:55 +0530

    Updating ReadMe changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="IdaiKuri",
-    version="0.0.1",
+    version="0.0.2",
     description="Generic Simple Template Engine for Filling and Parsing",
     py_modules=["IdaiKuri/Filler", "IdaiKuri/Parser"],
     package_dir={"": "SRCS"},
